d216dd2e8e86934a767e63dad9a2110dda6f6061
Tatsiana Kaltovich <tatyana.koltovich@bk.ru>
2024-04-08 03:51:16 +0200

    Lesson6_CreditCard

--- src/Lesson6/Lesson6_Task2.java
+++ src/Lesson6/Lesson6_Task2.java
@@ -1,5 +1,7 @@
 package Lesson6;
 
+import java.util.Scanner;
+
 public class Lesson6_Task2 {
     public static void main(String[] args) {
 //        Задача *:
@@ -10,7 +12,28 @@ public class Lesson6_Task2 {
 //        операции. При снятии денег, функция должна распечатывать каким количеством купюр
 //        какого номинала выдаётся сумма. Создать конструктор с тремя параметрами -
 //        количеством купюр каждого номинала.
+        Atm myAtm = new Atm(5, 3, 2);
+        myAtm.ShowInfomation();
 
+        System.out.println("Введите сумму, которую вы хотите внести на счет: ");
+        Scanner scanner = new Scanner(System.in);
+        int sum = scanner.nextInt();
+        myAtm.AddAmount(sum);
+        myAtm.ShowInfomation();
+
+        System.out.println("Введите сумму, которую вы хотите снять со счета: ");
+        Scanner scanner2 = new Scanner(System.in);
+        int sum2 = scanner2.nextInt();
+
+
+        boolean sucsess = myAtm.WithdrawAmount(sum2);
+        if (sucsess == true) {
+            System.out.println("Операция завершена успешно");
+        } else {
+            System.out.println("Операцию не удалось завершить успешно");
+        }
 
     }
+
+
 }
